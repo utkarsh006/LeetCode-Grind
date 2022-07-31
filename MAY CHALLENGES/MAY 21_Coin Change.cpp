578405429f169df189172c35f1d1cff0f3091578
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2022-07-31 13:05:31 +0530

    Update MAY 21_Coin Change.cpp

--- MAY CHALLENGES/MAY 21_Coin Change.cpp
+++ MAY CHALLENGES/MAY 21_Coin Change.cpp
@@ -1,4 +1,4 @@
-/* Problem ;
+/* Problem :
 You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 You may assume that you have an infinite number of each kind of coin.
