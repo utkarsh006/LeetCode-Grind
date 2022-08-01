a36894a16f3699d449cac87dd154446ac11ec44c
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2022-08-01 18:45:39 +0530

    Update JUL 16_Out of Boundary Paths.cpp

--- JULY CHALLENGES/JUL 16_Out of Boundary Paths.cpp
+++ JULY CHALLENGES/JUL 16_Out of Boundary Paths.cpp
@@ -46,25 +46,37 @@ If the adjacent cell is not discovered, we are out of bounds and can add another
 Finally, dp[r][c][MaxMove] will hold the number of ways to reach outside grid in maximum maxMove (r,c).
 */
 
-const int mod = 1e9 + 7, moves[4][2] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-int findPaths(int m, int n, int maxMove, int r, int c)
+class Solution 
 {
-	auto outOfBounds = [&](int m, int n, int r, int c)
+public:
+    const int mod = 1e9 + 7, moves[4][2] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+	int findPaths(int m, int n, int maxMove, int r, int c)
 	{
-	    return r < 0 || r >= m || c < 0 || c >= n;
-    };
-    // As the limitations are minimal enough, we may utilize a 51x51x51 dp matrix directly instead of a dynamic one.
-	uint dp[51][51][51]{};
-	// iterate for all available moves
-	for(int M = 1; M <= maxMove; M++)
-		for(int i = 0; i < m; i++)
-			for(int j = 0; j < n; j++)
-                // for each cell, try all 4 possible moves
-				for(int k = 0; k < 4; k++)
-					if(outOfBounds(m, n, i + moves[k][0], j + moves[k][1])) dp[i][j][M]++;
-					else dp[i][j][M] += dp[i + moves[k][0]][j + moves[k][1]][M-1] % mod;
-	return dp[r][c][maxMove] % mod;
-}
+		auto outOfBounds = [&](int m, int n, int r, int c)
+		{
+		    return r < 0 || r >= m || c < 0 || c >= n;
+	        };
+	        // As the limitations are minimal enough, we may utilize a 51x51x51 dp matrix directly instead of a dynamic one.
+		uint dp[51][51][51]{};
+		// iterate for all available moves
+		for(int M = 1; M <= maxMove; M++)
+		{
+			for(int i = 0; i < m; i++)
+			{
+				for(int j = 0; j < n; j++)
+				{
+					// for each cell, try all 4 possible moves
+					for(int k = 0; k < 4; k++)
+					{
+						if(outOfBounds(m, n, i + moves[k][0], j + moves[k][1])) dp[i][j][M]++;
+						else dp[i][j][M] += dp[i + moves[k][0]][j + moves[k][1]][M-1] % mod;
+					}
+				}
+			}
+	        }
+		return dp[r][c][maxMove] % mod;
+	}
+};
 /*
 Time Complexity : O(m * n * maxMove)
 Space Complexity : O(1)
