7cbfa37ce94475b9b102d78106bcebb1821f66c2
Rakesh Roshan <roshanrakesh7362@gmail.com>
2022-08-01 20:09:18 +0530

    Update APR27_Smallest String With Swaps.cpp

--- APR CHALLENGES/APR27_Smallest String With Swaps.cpp
+++ APR CHALLENGES/APR27_Smallest String With Swaps.cpp
@@ -6,7 +6,8 @@ Return the lexicographically smallest string that s can be changed to after usin
 
 // Approach 1 : Recursive
 
-class Solution {
+class Solution
+	{
     
     vector<int> p;
 
@@ -26,22 +27,26 @@ class Solution {
     }
 
 public:
-	string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+	string smallestStringWithSwaps(string s, vector<vector<int>>& pairs)
+	{
 		int n = s.size();
 		p = vector<int>(n, -1);
-		for (int i = 0; i<pairs.size(); i++) {
+		for (int i = 0; i<pairs.size(); i++)
+		{
 			Union(pairs[i][0], pairs[i][1]);
 		}
 
 		// set map
 		map<int, priority_queue<char, vector<char>, greater<char>>> m;
-		for (int i = 0; i<n; i++) {
+		for (int i = 0; i<n; i++)
+		{
 			m[Find(i)].push(s[i]);
 		}
 
 		// set ans
 		string ans = "";
-		for (int i = 0; i<n; i++) {
+		for (int i = 0; i<n; i++)
+		{
 			char now = m[Find(i)].top();
 			m[Find(i)].pop();
 			ans.push_back(now);
@@ -57,27 +62,30 @@ public:
 
 // Approach 2 : DFS Greedy Approach
 
-class Solution {
-public:
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+class Solution
+	{
+	public:
+    	string smallestStringWithSwaps(string s, vector<vector<int>>& pairs)
+	{
 
         vector<unordered_set<int>> g(s.size());
         for(const vector<int>& p: pairs)
             g[p[0]].insert(p[1]), g[p[1]].insert(p[0]);
 
-        vector<int> cc(s.size(), -1);
-        int ccid = 0;
+        vector<int> dfs(s.size(), -1);
+        int dfsid = 0;
         for(int i = 0; i < s.size(); i ++)
-            if(cc[i] == -1) dfs(g, i, ccid ++, cc);
+            if(dfs[i] == -1) dfs(g, i, dfsid ++, dfs);
 
-        vector<vector<int>> poses(ccid);
-        vector<string> order(ccid, "");
-        for(int i = 0; i < cc.size(); i ++)
-            poses[cc[i]].push_back(i), order[cc[i]] += s[i];
+        vector<vector<int>> poses(dfsid);
+        vector<string> order(dfsid, "");
+        for(int i = 0; i < dfs.size(); i ++)
+            poses[dfs[i]].push_back(i), order[dfs[i]] += s[i];
 
         for(string& s: order) sort(s.begin(), s.end());
 
-        for(int id = 0; id < ccid; id ++){
+        for(int id = 0; id < dfsid; id ++)
+	{
 
             int index = 0;
             for(int pos: poses[id]) s[pos] = order[id][index ++];
@@ -86,11 +94,11 @@ public:
     }
 
 private:
-    void dfs(const vector<unordered_set<int>>& g, int v, int ccid,
-             vector<int>& cc){
+    void dfs(const vector<unordered_set<int>>& g, int v, int dfsid,
+             vector<int>& dfs){
 
-        cc[v] = ccid;
+        dfs[v] = dfsid;
         for(int w: g[v])
-            if(cc[w] == -1) dfs(g, w, ccid, cc);
+            if(dfs[w] == -1) dfs(g, w, dfsid, dfs);
     }
 };
