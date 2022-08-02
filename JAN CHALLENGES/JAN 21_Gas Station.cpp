0acc7dcb51b2c21e2c924141b29ab5ccc406a733
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2022-08-02 17:47:45 +0530

    Update JAN 21_Gas Station.cpp

--- JAN CHALLENGES/JAN 21_Gas Station.cpp
+++ JAN CHALLENGES/JAN 21_Gas Station.cpp
@@ -5,6 +5,19 @@ You begin the journey with an empty tank at one of the gas stations.
 Given two integer arrays gas and cost, return the starting gas station's index
 if you can travel around the circuit once in the clockwise direction, otherwise return -1.
 If there exists a solution, it is guaranteed to be unique.
+
+Example 1:
+
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
 */
 
 /* Note:
