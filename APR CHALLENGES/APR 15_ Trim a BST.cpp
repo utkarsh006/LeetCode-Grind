416a58bf70dc83f1d2552059d6c8a3e2399d123f
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2022-08-05 14:37:21 +0530

    Update APR 15_ Trim a BST.cpp

--- APR CHALLENGES/APR 15_ Trim a BST.cpp
+++ APR CHALLENGES/APR 15_ Trim a BST.cpp
@@ -1,3 +1,22 @@
+/* Problem :
+
+*/
+
+/* Approach :
+Only nodes in the supplied range from low to high should be included in our binary search tree.
+
+1. Making a recursive function, we'll determine which nodes should be returned in post-order.
+
+2. Therefore, if my current node value in a binary search tree is less than low, 
+we may ignore the left subtree (a property of binary search trees: the left subtree of a node only includes nodes with values lower than the node's key).
+
+3. Similar to this, we can disregard the right subtree if my current node value is higher than high 
+(a characteristic of a binary search tree is that the right subtree of a node only includes nodes with values higher than the node's key).
+
+4. if the range of our current node value is inside it.
+The current node can then be returned. Its left and right subtrees have previously been figured out.
+*/
+
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high)
@@ -19,3 +38,8 @@ public:
         return NULL;
     }
 };
+
+/*
+Time Complexity : O(N)
+Space Complexity : Recursive stack space
+*/
