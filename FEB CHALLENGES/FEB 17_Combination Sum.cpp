372a9bc09bdda3931696a3153270fe27b78418bc
tanya-gupta25 <gtanya729@gmail.com>
2022-08-21 23:08:03 +0530

    corrected spelling and adjusted text

--- FEB CHALLENGES/FEB 17_Combination Sum.cpp
+++ FEB CHALLENGES/FEB 17_Combination Sum.cpp
@@ -9,7 +9,7 @@ It is guaranteed that the number of unique combinations that sum up to target is
 */
 
 /*
-Examples: 
+Examples:
 
     Example 1:
         Input: candidates = [2,3,6,7], target = 7
@@ -30,58 +30,67 @@ Examples:
 */
 
 /*
-Approach 
+Approach
 
 1. Here we have to find out every unique combination sum possible which will be equal to target.
 
-2. We'll check for every element of array , if that element in less than target then we'll move forward with considering the combination,otherwise will backtrack.
+2. We'll check for every element of array , if that element in less than target then we'll move forward with
+    considering the combination,otherwise will backtrack.
 
-3. Instead of adding  sum in different variable we have subtracted candidates[i] from target so our target will keep on decreasing with every valid combination.
+3. Instead of adding  sum in different variable we have subtracted candidates[i] from target so our target
+   will keep on decreasing with every valid combination.
 
-4. If candidates[i] is greater than target then we will simply backtrack and will call function for next value, because adding further will not give us any combination.
+4. If candidates[i] is greater than target then we will simply backtrack and will call function for
+   next value, because adding further will not give us any combination.
 
-5. f candidates[i] is equal to target then we will store the result as our combination & will not move forward because it will only exceed the sum.
+5. If candidates[i] is equal to target then we will store the result as our combination & will
+   not move forward because it will only exceed the sum.
 
-6.If candidates[i] is less than target then we will add the element in the vector and will again call for the same index and will subtract the element from target, while backtracking we will pop the element from the vector and will call for next index.
+6.If candidates[i] is less than target then we will add the element in the vector and will again
+  call for the same index and will subtract the element from target, while backtracking we will pop the element 
+  from the vector and will call for next index.
 
-7. Base case : If our target becomes 0 that means we got a combination and we will add our vector in the final vector as answer and will return.
- 
-8. Even if our index reached equal to the size of vector then also we will check if our target is 0 then add vector in ans vector otherwise if target is not 0 then simply return.
+7. Base case : If our target becomes 0 that means we got a combination and we will add our
+    vector in the final vector as answer and will return.
+
+8. Even if our index reached equal to the size of vector then also we will check if our target
+    is 0 then add vector in ans vector otherwise if target is not 0 then simply return.
 
 */
 
-class Solution {
+class Solution
+{
 public:
-    
-   void combination(vector<int>& candidates,int target,vector<vector<int>>&ans,vector<int>&v,int i)
+    void combination(vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &v, int i)
     {
-        if(target==0)
+        if (target == 0)
         {
             ans.push_back(v);
             return;
         }
-        if(i==candidates.size())
+        if (i == candidates.size())
         {
-            if(target==0)
+            if (target == 0)
             {
                 ans.push_back(v);
             }
-            else return;
+            else
+                return;
         }
-         if(candidates[i]<=target)   
-         {
-                v.push_back(candidates[i]);
-                combination(candidates,target-candidates[i],ans,v,i);
-                v.pop_back();  
-         }  
-        
-        combination(candidates,target,ans,v,i+1);
-        
+        if (candidates[i] <= target)
+        {
+            v.push_back(candidates[i]);
+            combination(candidates, target - candidates[i], ans, v, i);
+            v.pop_back();
+        }
+
+        combination(candidates, target, ans, v, i + 1);
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+    {
         vector<vector<int>> ans;
-        vector<int>v;
-        combination(candidates,target,ans,v,0);
+        vector<int> v;
+        combination(candidates, target, ans, v, 0);
         return ans;
     }
 };
