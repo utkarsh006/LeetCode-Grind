74908e257f8ef342feef073df63b2a6e88445862
tanya-gupta25 <gtanya729@gmail.com>
2022-08-21 23:09:26 +0530

    Update FEB 17_Combination Sum.cpp

--- FEB CHALLENGES/FEB 17_Combination Sum.cpp
+++ FEB CHALLENGES/FEB 17_Combination Sum.cpp
@@ -1,11 +1,14 @@
 /*
 Problem: Combination Sum
 
-Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+Given an array of distinct integers candidates and a target integer target, return a list of all unique 
+combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
 
-The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique 
+if the frequency of at least one of the chosen numbers is different.
 
-It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 
+combinations for the given input.
 */
 
 /*
@@ -96,7 +99,8 @@ public:
 };
 
 /*
-Time Complexity: O((2^target)*k) {here suppose target is 10 then we can take an element as 10 times which is equal to target so 2^target and  k is average length which it will take for every recursion call}
+Time Complexity: O((2^target)*k) {here suppose target is 10 then we can take an element as 10 times which 
+is equal to target so 2^target and  k is average length which it will take for every recursion call}
 
 Space Complexity:O(k* no. of combinations)
 */
