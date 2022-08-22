e0c20f7b1b1556c47714d29ec095fdcd8b15d7b1
Surjyanee Halder <75573569+surjyaneeh@users.noreply.github.com>
2022-08-22 16:33:50 +0530

    Update AUG 22_Power of Four.cpp

--- AUG CHALLENGES/AUG 22_Power of Four.cpp
+++ AUG CHALLENGES/AUG 22_Power of Four.cpp
@@ -22,21 +22,27 @@ Output: true
 
 
 /* Approach :
-How do we determine if a number is a power of four? Try to divide it by 4 and see if we still have 1 after numerous divisions.
-This is a simple procedure, however the complexity will be O (log n). There is a better method.
+How do we determine if a number is a power of four? 
+Try to divide it by 4 and see if we still have 1 after numerous divisions.
+This is a simple procedure, however the complexity will be O (log n). 
+There is a better method.
 We must look for 3 things in order to determine whether a number is a power of 4:
 
 1. The value is positive.
 2. Number is a power of two.
 3. The power of this two is even power.
 
-The first requirement is simple. The x&(x-1) method, which eliminates the final significant bit from a binary representation,
-    can be used to solve the second requirement. For instance, 11010 & 11001 = 11000. If a number has just one significant bit,
-    it is a power of two, hence after this operation, it must equal zero.
+The first requirement is simple. The x&(x-1) method, 
+ which eliminates the final significant bit from a binary representation,
+ can be used to solve the second requirement. For instance, 11010 & 11001 = 11000. 
+ If a number has just one significant bit,
+ it is a power of two, hence after this operation, it must equal zero.
 
-The final step can be challenging. Hopefully, by the time we get to this stage, we already know that the number is a power of 2,
-    leaving us with only a limited number of options: 1, 10, 100, 1000,... How can we tell one half of them (odd powers) from from the other?
-    Utilizing the binary mask m = 1010101010101010101010101 is the trick.
+The final step can be challenging. 
+ Hopefully, by the time we get to this stage, we already know that the number is a power of 2,
+ leaving us with only a limited number of options: 1, 10, 100, 1000,... 
+ How can we tell one half of them (odd powers) from from the other?
+ Utilizing the binary mask m = 1010101010101010101010101 is the trick.
 */
 
 
