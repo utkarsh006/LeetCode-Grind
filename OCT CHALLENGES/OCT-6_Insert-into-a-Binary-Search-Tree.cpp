1dc037424dd19445b8735e60a25e95d81ac0284c
kalash <kalashvasaniya@gmail.com>
2022-10-12 21:47:29 +0530

    update

--- OCT CHALLENGES/OCT-6_Insert-into-a-Binary-Search-Tree.cpp
+++ OCT CHALLENGES/OCT-6_Insert-into-a-Binary-Search-Tree.cpp
@@ -1,3 +1,13 @@
+//701. Insert into a Binary Search Tree
+//link of ps :- https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+/*
+The main logic for the problem is at every node we check to make sure it's bigger or smaller than the value we are trying to insert. If the value is bigger we go right...otherwise we go left. What's really important to understand is we are also verifying the node will be valid when we insert it...as we traverse down the tree we are checking all the nodes along the eventual path the node will take..
+
+Time Complexity = log(n)
+Space Complexity = O(1)
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
