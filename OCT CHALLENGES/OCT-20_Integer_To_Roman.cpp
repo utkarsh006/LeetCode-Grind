2307091850c225a09af7ee86b7b290d05b09dbbe
Nishkarsh Saxena <nishkarshsaxena800@gmail.com>
2022-10-21 13:13:12 +0530

    Added 20th October Leetcode Daily Challenge

---
+++ OCT CHALLENGES/OCT-20_Integer_To_Roman.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string intToRoman(int num) 
+    {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    }
+};
