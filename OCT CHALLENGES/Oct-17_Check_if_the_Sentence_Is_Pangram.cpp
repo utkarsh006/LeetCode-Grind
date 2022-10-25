4383a43bdc71846caec0b0f97556be4247a844db
Anjali-Chaudhary3 <96697247+Anjali-Chaudhary3@users.noreply.github.com>
2022-10-25 16:04:29 +0530

    Updated

--- OCT CHALLENGES/Oct-17_Check_if_the_Sentence_Is_Pangram.cpp
+++ OCT CHALLENGES/Oct-17_Check_if_the_Sentence_Is_Pangram.cpp
@@ -1,10 +1,10 @@
-Question : A pangram is a sentence where every letter of the English alphabet appears at least once.
+/*Question : A pangram is a sentence where every letter of the English alphabet appears at least once.
 
 Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise
 
 Approach :
 Since no repetition is allowed so we can put all the characters of the string in the set .
-If the set size is 26 then only it is a valid string otherwise not.
+If the set size is 26 then only it is a valid string otherwise not.*/
   
   
 Soution:
@@ -18,4 +18,6 @@ public:
     
 };
 
-  
+/* Time complexity: O(1)
+   Space Complexity : O(n)
+*/
