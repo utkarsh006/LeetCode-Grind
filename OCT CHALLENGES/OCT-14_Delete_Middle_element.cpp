6d95e1a74ce4770b9c951433ee7a3d0ba212aefc
Riya Bhaskar <riyabhaskar2@gmail.com>
2022-10-31 22:23:29 +0530

    oct 14 corrected

--- OCT CHALLENGES/OCT-14_Delete_Middle_element.cpp
+++ OCT CHALLENGES/OCT-14_Delete_Middle_element.cpp
@@ -16,22 +16,76 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
-public:
-    ListNode *deleteMiddle(ListNode *head)
-    {
-        if (head == nullptr || head->next == nullptr)
-            return nullptr;
-        ListNode *slow = head, *fast = head;
-        ListNode *prev = nullptr;
-        while (fast != nullptr && fast->next != nullptr)
-        {
-            prev = slow;
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        prev->next = slow->next;
-        return head;
-    }
+
+#include <bits stdc++.h="">
+using namespace std;
+struct Node {
+    int val;
+    struct Node* next;
 };
+
+int count_nodes(struct Node* head)
+{
+    int n_count = 0;
+    while (head != NULL) {
+        head = head->next;
+        n_count++;
+    }
+    return n_count;
+}
+
+struct Node* delete_middle(struct Node* head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL) {
+        delete head;
+        return NULL;
+    }
+    struct Node* Head_copy = head;
+    // total nodes currently there in the list
+    int count = count_nodes(head);
+    // position of middle element in the list
+    int mid = count / 2;
+    // Delete the middle node
+    while (mid-- > 1) {
+        head = head->next;
+    }
+    // Delete the middle node
+    head->next = head->next->next;
+    return Head_copy;
+}
+void display_List(struct Node* x)
+{
+    while (x != NULL) {
+        cout << x->val << "->";
+        x = x->next;
+    }
+    // Last element points to null
+    cout << "NULL\n";
+}
+Node* newNode(int value)
+{
+    struct Node* t = new Node;
+    t->val = value;
+    t->next = NULL;
+    return t;
+}
+
+int main()
+{
+    // Adding elements to the empty list
+    struct Node* head = newNode(5);
+    head->next = newNode(10);
+    head->next->next = newNode(15);
+    head->next->next->next = newNode(20);
+     head->next->next->next->next = newNode(25);
+    cout << "Original List" << endl;
+    display_List(head);
+    head = delete_middle(head);
+    cout << "List after deleting the middle element"
+ << endl;
+    display_List(head);
+    return 0;
+}
+</bits>
