a09dae64cc42eb128b9babddb2bc345301ba8c7f
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-06-08 07:59:45 +0530

    Create Jun_06_Can Make Arithmetic Progression From Sequence.cpp

---
+++ 2023 Daily Challenges/JUNE 2023 CHALLENGES/Jun_06_Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) 
+    {
+        sort(arr.begin(),arr.end());
+        int diff = arr[1] - arr[0];
+        for(int i = 2; i<arr.size();i++)
+        {
+            if(arr[i]-arr[i-1] != diff)
+              return false;
+        }
+        return true;
+    }
+};
